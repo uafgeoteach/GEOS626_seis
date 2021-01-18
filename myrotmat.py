0abc40b1a6bcc762ad5929cd02b209820f283107
Nealey Sims <nealeysims@Nealeys-MBP.wirelessinternet>
2021-01-18 14:16:25 -0900

    template files for hw_mt

---
+++ myrotmat.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def myrotmat(a,b):
+    
+    c=a+b
+    
+    return c
---
+++ myrotmat_gen.py
@@ -0,0 +1,10 @@
+import numpy as np
+import math as m
+from numpy import linalg as LA
+from rotmat import rotmat
+
+def myrotmat_gen(a,b):
+    
+    c=a+b
+    
+    return c
