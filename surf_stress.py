c92d2edf8af680bef0e3214d0d07f9b402ab3a74
Carl Tape <carltape@gmail.com>
2021-02-25 00:29:02 +0000

    add comment about tolerance

--- surf_stress.py
+++ surf_stress.py
@@ -27,9 +27,9 @@ def surf_stress(f):
     spshell_config.omega = 2*np.pi*f # angular frequency for stress_disp_tor
     
     # note: the dimension of rvec and WT is the number of points needed for
-    # the numerical integration -- this will vary. You can adjust it via the 'max_step' parameter
+    # the numerical integration -- this will vary. You can adjust it via the 'max_step' parameter (try 1E2 or 1E3)
     rspan_t = tuple(spshell_config.rspan.tolist())
-    sol = solve_ivp(stress_disp_tor,rspan_t,WT0,max_step=5.3*1E4)
+    sol = solve_ivp(stress_disp_tor,rspan_t,WT0,max_step=5E4)
     WT = sol.y
     rvec = sol.t
     spshell_config.WT = WT
