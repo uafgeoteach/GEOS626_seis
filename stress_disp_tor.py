7ad4719e2023d7c3f8619d5193407fbfc191fab5
Liam Toney <ldtoney@alaska.edu>
2021-04-05 04:35:06 +0000

    Remove omega global

--- stress_disp_tor.py
+++ stress_disp_tor.py
@@ -2,7 +2,7 @@ import numpy as np
 from earthfun import earthfun
 import spshell_config
 
-def stress_disp_tor(r, WT_0):
+def stress_disp_tor(r, WT_0, omega):
     """ Python adaptation of stress_disp_tor.m by Carl Tape
         Coding by Amanda McPherson, Dec 2020
         
@@ -11,12 +11,11 @@ def stress_disp_tor(r, WT_0):
         INPUT:
             r - radius at which to evaluate the derivatives 
             WT_0 - numpy array. WT_0[0] is W(r) and WT_0[1] is T(r)
+            omega - angular frequency
             
         OUTPUT:
             dWT - numpy array. dWT[0] is the derivative of W(r), and dWT[1] is the derivative of T(r) """
     
-    #global omega, l, imod, rho, mu
-    omega = spshell_config.omega
     l = spshell_config.l
     imod = spshell_config.imod
     rho = spshell_config.rho
--- surf_stress.py
+++ surf_stress.py
@@ -3,6 +3,7 @@ from scipy.integrate import solve_ivp
 from stress_disp_tor import stress_disp_tor
 import spshell_config
 
+
 def surf_stress(f):
     """ Python adaptation of surf_stress.m by Carl Tape
         Coding by Amanda McPherson, Dec 2020
@@ -17,19 +18,16 @@ def surf_stress(f):
             WT[1,-1} = stress value at the earth's surface (r = rspan[1])
             
         UPDATES GLOBAL VARIABLES:
-            omega = angular frequency for stress_disp_tor
             rvec = radii at which displacement and stress eigenfunctions were evaluated at
             WT = displacement and stress eigenfunctions"""
     
-    #global omega, rvec, rspan, WT
-    
     WT0 = np.array([1.0, 0.0]) # initial values of [displacement, stress]
-    spshell_config.omega = 2*np.pi*f # angular frequency for stress_disp_tor
+    omega = 2*np.pi*f # angular frequency for stress_disp_tor
     
     # note: the dimension of rvec and WT is the number of points needed for
     # the numerical integration -- this will vary. You can adjust it via the 'max_step' parameter (try 1E2 or 1E3)
     rspan_t = tuple(spshell_config.rspan.tolist())
-    sol = solve_ivp(stress_disp_tor,rspan_t,WT0,max_step=5E4)
+    sol = solve_ivp(stress_disp_tor,rspan_t,WT0,max_step=5E4, args=(omega,))
     WT = sol.y
     rvec = sol.t
     spshell_config.WT = WT
