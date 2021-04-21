e13beb14118ce58d4d7f227a79290eb14f0f74e2
Carl Tape <carltape@gmail.com>
2021-04-21 06:20:33 +0000

    default max_step 5e3

--- surf_stress_love.py
+++ surf_stress_love.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.integrate import solve_ivp
 from stress_disp_love import stress_disp_love
 
-def surf_stress_love(k0, omega, rspan, crho, cmu, mrho, mmu, cthick, max_step=5.3e3, return_wt_rvec=False):
+def surf_stress_love(k0, omega, rspan, crho, cmu, mrho, mmu, cthick, max_step=5e3, return_wt_rvec=False):
     """
     surf_stress_love() calculates the stress at the surface of a layer-over-halfspace
 
@@ -24,27 +24,27 @@ def surf_stress_love(k0, omega, rspan, crho, cmu, mrho, mmu, cthick, max_step=5.
         [rvec = radii at which displacement and stress eigenfunctions were evaluated at]
     
     Applied Seismology (GEOS 626) University of Alaska Fairbanks
-    contributors: Charles Ammon, Carl Tape, Amanda McPherson, Liam Toney
+    contributors: Charles Ammon, Carl Tape, Amanda McPherson*, Liam Toney
     """
 
     k = k0
 
     # calculate initial conditions at r=0 within the mantle halfspace
     mbeta = np.sqrt(mmu/mrho)
-    mk = omega/mbeta
-    nub = np.sqrt(k**2 - mk**2)
+    mk    = omega/mbeta
+    nub   = np.sqrt(k**2 - mk**2)
 
     if np.iscomplex(nub):
         print('setting nub=0 (k=%.3e mk=%.3e)'% (k,mk))
         nub = 0
 
     Tbot = mmu*nub
-    WT0 = np.array([1.0, Tbot])   # the initial values of [displacement stress]
+    WT0  = np.array([1.0, Tbot])   # the initial values of [displacement stress]
 
     # note: the dimension of rvec and WT is the number of points needed for
     # the numerical integration -- this will vary. You can adjust it via the 'max_step' parameter
     rspan_t = tuple(rspan.tolist())
-    sol = solve_ivp(stress_disp_love,rspan_t,WT0,max_step=max_step, args=(k, omega, rspan, crho, cmu, mrho, mmu, cthick))
+    sol = solve_ivp(stress_disp_love, rspan_t, WT0, max_step=max_step, args=(k, omega, rspan, crho, cmu, mrho, mmu, cthick))
     WT = sol.y
     rvec = sol.t
 
