00dbe6a9f7f3be66d8c419ad9c88f749f2ff55b2
Aakash Gupta <aakash10gupta@gmail.com>
2023-01-04 04:42:28 -0900

    added libraries compiled from functions provided with the templates

---
+++ lib_geos.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+# Library of functions for the GEOS 626 and GEOS 627 courses at UAF
+# Contributers: Carl Tape, Nealey Sims*, Amanda McPherson*, Aakash Gupta*
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+###################################################################
+
+def fftvec(t):
+    # Python version of fftvec.m made by Carl tape
+    # FFTVEC provides frequency vector for Matlab's fft convention
+    # 
+    # This is the bare-bones FFT example for teaching purposes in the sense
+    # that there is no padding with zeros, no odd-number time series, no
+    # all-positive frequencies, and no use of fftshift.
+    #
+    # EXAMPLE:
+    #   t = np.arange(2,9.0,0.4); n=len(t); f = fftvec(t); ix = 4; 
+    #   dt = t[1]-t[0]; df = 1/(2*dt)/(n/2) 
+    #   fig=plt.figure();plt.plot(f,'-'); 
+    #   fig2=plt.figure(); plt.plot(np.fft.fftshift(f),'-');
+    #
+    # NOTE: I chose to set fNyq as the negative frequency such that
+    #       fftshift(fftvec(t)) will give a vector increasing uniformly from
+    #       -fNyq to just less than fNyq, with f=0 at index n/2+1
+    #
+    # NOTE: If you want to consider all-positive frequencies, then use abs(f)
+    # 
+    n = len(t)
+    if n % 2 == 1:
+       print('error(time series must have an even number of points)')
+
+    dt = t[2] - t[1];       # sample rate
+    fNyq = 1/(2*dt);        # Nyquist frequency
+    
+    # first half of frequency vector (note: first entry is f=0)
+    f1 = np.transpose(np.linspace(0, float(fNyq), int((n/2)+1)));
+    # full frequency vector
+    #f = [f1 ; -f1(end-1:-1:2)];        % fNyq > 0
+    
+    f = np.concatenate([f1[0:int(n/2)] , -f1[:0:-1]])    # fNyq < 0
+    return f
+    
+    # alternatively (for fNyq < 0)
+    #df = 1/(n*dt);          % =2*fNyq/n
+    #f1 = linspace(-fNyq,fNyq-df,n)'
+    #f = [f1(n/2+1:n) ; f1(1:n/2)];
+
+    #==========================================================================
+    
+####################################################
+
+def gridvec(xmin,xmax,numx,ymin,ymax):
+    """  This function inputs specifications for creating a grid 
+         of uniformly spaced points, reshaped into column vectors
+         of the x- and y-coordinates.  Note that dx = dy."""
+    
+    xvec0 = np.linspace(xmin,xmax,numx)
+    dx = xvec0[1] - xvec0[0]
+    yvec0 = np.arange(ymin, ymax+dx, dx)
+    
+    X, Y = np.meshgrid(xvec0,yvec0)
+    a,b = X.shape
+    xvec = np.reshape(X,(a*b,1))
+    yvec = np.reshape(Y,(a*b,1))
+    numy = len(yvec0)
+    return xvec, yvec, numy, X, Y
+
+#def gridvec(xmin,xmax,numx,ymin,ymax,returnXY=False):
+#    """  This function inputs specifications for creating a grid 
+#         of uniformly spaced points, reshaped into column vectors
+#         of the x- and y-coordinates.  Note that dx = dy."""
+#    
+#    xvec0 = np.linspace(xmin,xmax,numx)
+#    dx = xvec0[1] - xvec0[0]
+#    yvec0 = np.arange(ymin, ymax+dx, dx)
+#    
+#    X, Y = np.meshgrid(xvec0,yvec0)
+#    a,b = X.shape
+#    xvec = np.reshape(X,(a*b,1))
+#    yvec = np.reshape(Y,(a*b,1))
+#    
+#    numy = len(yvec0)
+#    
+#    if returnXY:
+#        return xvec, yvec, numy, X, Y
+#    else:
+#        return xvec, yvec, numy
+
+####################################################
+
+def plot_histo(hdat,edges,itype=2,make_plot=True):
+    #PLOT_HISTO plot a histogram with cyan bars and black boundaries
+    #
+    # INPUT
+    #   hdat        input data to bin
+    #   edges       vector defining the edges of the bins (for hdat)
+    #   itype       optional: type of histogram (=1,2,3) [default = 2]
+    #   make_plot   optional: plot histogram [default = true]
+    #hdat = hdat.flatten();
+    #barcolor = [1, 1, 1]*0.8;
+    
+    # bin width (only relevant if bins are the same width)
+    dbin   = edges[1] - edges[0]
+    hedges = np.append(edges,edges[-1]+dbin)
+    Ntotal = len(hdat);
+    # key command
+    N,b = np.histogram(hdat,hedges);
+
+    if itype ==1:
+        Nplot = N; xlab = 'Count'
+    if itype ==2: 
+        Nplot = np.divide(N,Ntotal); xlab = 'Fraction'
+    if itype ==3: 
+        Nplot = np.divide(np.divide(N,Ntotal),dbin); xlab = 'PDF'
+        #if len(unique(edges)) > 1:
+        if np.std(np.diff(edges))/np.mean(np.diff(edges)) > 1e-4:       # ad hoc criterion
+            print(np.unique(np.diff(edges)))
+            print('PDF is not implemented to allow bins with varying widths')
+            
+    elif itype!=1 and itype!=2 and itype!=3: 
+        print('itype = %i -- it must be 1,2, or 3'%(itype)) 
+
+    if make_plot==True:
+        plt.bar(edges, Nplot, width=0.8*dbin, align='edge');
+        plt.xlim([min(edges), max(edges)]);
+        plt.ylabel('%s (N=%i)'% (xlab,Ntotal))
+        
+        if len(hdat) != np.sum(N):
+            print('(plot_histo): You may want to extend the histogram edges -->');
+            print(' there are %i/%i input that are outside the specified range'%
+                (len(hdat)-np.sum(N),len(hdat)))
+            #disp(sprintf(' the number of input (%i) does not equal the sum of bin counts (%i).',length(hdat),sum(N)));
+    
+    plt.tight_layout()
+    
+###########################################################
---
+++ lib_seis.py
@@ -0,0 +1,365 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+# Library of functions for the GEOS 626 course at UAF
+# Contributers: Carl Tape, Nealey Sims*, Amanda McPherson*, Aakash Gupta*
+
+import datetime as dt
+import matplotlib.pyplot as plt
+import math as m
+import numpy as np
+import scipy.io
+
+from itertools import product, combinations
+
+###################################################################
+
+def Bkspline(clon, clat, q, lon_vec, lat_vec, ncol=1):
+    """ INPUT:
+            clon, clat, q   = these describe the local spherical spline basis function
+            ncol            = how many columns of ff you want returned (derivatives)
+                              (=1 default)
+                              Can be 1, or 3,4,5
+            lon_vec,lat_vec = datapoints at which you want the spherical spline evaluated
+
+        OUTPUT:
+            ff              = value of the spline function (and derivatives)
+                              evaluated at the specified lon-lat points"""
+    
+    # allow for scalar input values
+    lon_vec = np.atleast_1d(lon_vec)
+    lat_vec = np.atleast_1d(lat_vec)
+    
+    # convert to theta-phi
+    deg = 180/np.pi
+    ph     = clon/deg
+    th     = (90-clat)/deg
+    ph_vec = lon_vec/deg
+    th_vec = (90-lat_vec)/deg
+    
+    # options and parameters -- q controls the scale (width) of the spline
+    nf    = 2**q
+    c72   = np.cos(72/deg)
+    base  = np.arccos(c72 / (1 - c72))
+    db    = base / nf
+    zeps  = 1e-3*base     # determines whether a datapoint is ON a gridpoint
+    
+    # datapoint locations
+    costh = np.cos(th_vec)
+    sinth = np.sin(th_vec)
+    ndata = len(th_vec)
+    
+    # r : delta/delta-bar in WD95
+    delta = np.arccos( np.cos(th) * costh + np.sin(th) * sinth * np.cos(ph - ph_vec) )
+    r   = delta/ db
+    dif = r - 1
+    
+    # separate r into three parts: assign each element to one of four regions
+    inds1 = np.flatnonzero(dif > 1)     # outside outer circle
+    inds2 = np.flatnonzero((dif <= 1) & (dif >= 0))  # within outer ring
+    inds3 = np.flatnonzero((dif > -1 + zeps) & (dif < 0))  # within inner circle
+    inds4 = np.flatnonzero(dif <= -1 + zeps)   # ON the center point
+    
+    # check
+    if len(inds1) + len(inds2) + len(inds3) + len(inds4) - len(dif) != 0:
+        print(len(inds1))
+        print(len(inds2))
+        print(len(inds3))
+        print(len(inds4))
+        print(len(inds1)+ len(inds2) + len(inds3) + len(inds4))
+        print(len(dif))
+        raise Exception('Data points have not been partitioned correctly')
+        
+    if ncol == 1:
+        ff = np.zeros((ndata,1))
+        if len(inds2) != 0:
+            ff[inds2] = ((-0.25*dif[inds2]  + 0.75)*dif[inds2]  - 0.75)*dif[inds2] + 0.25
+        
+        if len(inds3) != 0:
+            ff[inds3] = (0.75*r[inds3] - 1.5) * r[inds3]**2  + 1
+            
+        if len(inds4) != 0:
+            ff[inds4] = 1
+        
+    else:
+        cosdel = np.cos(th)*costh + np.sin(th) * sinth * np.cos(ph - ph_vec)
+        sindel = np.sqrt(1 - cosdel*cosdel)
+        cotdel = cosdel / sindel
+        
+        # ddelta/dphi and ddelta/dtheta (see MMA file wang_arc.nb)
+        dadp = ( np.sin(th) * sinth * np.sin(ph_vec - ph) ) / sindel
+        dadt = ( np.cos(th) * sinth - costh * np.sin(th) * np.cos(ph - ph_vec) ) / sindel
+        
+        # db : delta-bar in WD95
+        # d_near varies for each gridpoint, due to irregularities in grids
+        dq = 1 / db
+        
+        # columns of ff :
+        # (1) f, function value
+        # (2) df/dph
+        # (3) df/dth
+        # (4) surf_del2 -- depends only on delta
+        # (5) |del f|   -- depends only on delta
+    
+        # datapoint is outside the outer circle
+        ff = np.zeros((ndata,ncol))
+        
+        # datapoint is within the outer ring
+        ff[inds2,0] = np.ravel(((-0.25*dif[inds2] + 0.75)*dif[inds2] - 0.75) * dif[inds2] + 0.25)
+        ff[inds2,1] = np.ravel(dq * (-0.75 + 1.5*dif[inds2] - 0.75*dif[inds2]**2) * dadp[inds2])
+        ff[inds2,2] = np.ravel(dq * (-0.75 + 1.5*dif[inds2] - 0.75*dif[inds2]**2) * dadt[inds2])
+        
+        if ncol >= 4:
+            ff[inds2,3] = np.ravel(dq * (3 - 1.5*r[inds2] + cotdel[inds2] * (-0.75 + 1.5*dif[inds2] - 0.75*dif[inds2]**2)))
+            ff[inds2,4] = np.ravel(0.75 * db**-3 * (2*db - delta[inds2])**2)
+            
+        # datapoint is within the inner circle
+        ff[inds3,0] = np.ravel((0.75*r[inds3] - 1.5) * (r[inds3]**2) + 1)
+        ff[inds3,1] = np.ravel(dq * (-3*r[inds3] + 2.25*r[inds3]**2) * dadp[inds3])
+        ff[inds3,2] = np.ravel(dq * (-3*r[inds3] + 2.25*r[inds3]**2) * dadt[inds3])
+        
+        if ncol >= 4:
+            ff[inds3,3] = np.ravel(dq * (-3 + 4.5*r[inds3] + cotdel[inds3] * (-3*r[inds3] + 2.25*r[inds3]**2)))
+            ff[inds3,4] = np.ravel(0.75 * db**-3 * (4*db - 3*delta[inds3]) * delta[inds3])
+            
+        # datapoint is in the vicinity of the target spline centerpoint
+        # FIX THIS : see Wang & Dahlen (1995)
+        # here we simply assign it the closest value
+        
+        if len(inds4) > 0:
+            if ncol > 3:
+                igood = np.nonzero(dif > -1 + zeps)
+                imin  = np.amin(r[igood])
+                d2val = ff[imin,3]
+                tvec = np.zeros((1,ncol))
+                tvec[0] = 1
+                tvec[-1] = d2val
+                ff[inds4,0] = np.matlib.repmat(tvec,len(inds4),1)
+                
+            elif ncol == 3:
+                ff[inds4,0:3] = np.array([1, 0, 0])
+                
+            elif ncol == 1:
+                ff[inds4,0] = 1
+                
+    return ff
+
+###################################################################
+
+def globefun3(R,lat,lon,bool_point,lc, fig,ax):
+    # Python adaptation of globefun3.m
+    # Python coding done by Nealey Sims
+    #
+    # GLOBEFUN3 plots a single point on the sphere with a reference globe
+    #
+    # INPUT
+    #   R           radius of sphere
+    #   lat         latitude (deg)  (theta = 90 - lat)
+    #   lon         longitude (deg) (lon = phi)
+    #   bool_point  plot the specified point
+    #   lc          line style/color
+    #
+    #
+    
+    deg = 180/np.pi
+    lw = 1.0
+    th = (90-lat)/deg
+    ph = lon/deg
+    
+    # (1) xy-plane circle (equator)
+    # (2) yz-plane
+    # (3) xz-plane (prime meridian)
+    gpts = 100
+    tt = np.linspace(0, 2*np.pi, gpts)
+    
+    
+    ax.set_box_aspect((4,4,3.4))  ## Equal axis
+    ax.plot(R*np.cos(tt), R*np.sin(tt), np.zeros(gpts), lc, linewidth=lw)
+    ax.plot(np.zeros(gpts), R*np.cos(tt), R*np.sin(tt), lc, linewidth=lw)
+    ax.plot(R*np.cos(tt), np.zeros(gpts), R*np.sin(tt), lc, linewidth=lw)
+    
+    fac = 1.15; lw2 = 0.5
+    RR = np.array([-R,R])
+    ax.plot((fac*RR),[0, 0],[0, 0],'k', linewidth=lw2)
+    ax.plot([0, 0],(fac*RR),[0, 0],'k', linewidth=lw2)
+    ax.plot([0, 0],[0, 0],(fac*RR),'k', linewidth=lw2)
+    
+    # draw cube
+    r = (fac*RR)
+    for s, e in combinations(np.array(list(product(r, r, r))), 2):
+        if np.sum(np.abs(s-e)) == r[1]-r[0]:
+            ax.plot3D(*zip(s, e), color="k", linewidth=lw2)
+    
+    
+    if bool_point == 1:
+        # plot the specified point P(r,th,phi)
+        msize = 20; lw3 = lw+0.5
+        # [X,Y,Z] = sph2cart(az,el,r)
+        [X,Y,Z] = sph2cart(ph,np.pi/2-th,R)  #Note: need sph2cart function in folder
+        
+        [X2,Y2,Z2] = sph2cart(ph,0,R)
+  
+        ax.plot([X],[Y],[Z], color=lc, marker='o')
+        ax.plot([0, X],[0, Y],[0, Z],lc, linewidth= lw3)
+        
+        leps = 0.1;
+        if ( lat >= -90+leps) and (lat <= 90-leps):
+            xzplane = np.array([R*np.cos(tt) , np.zeros(gpts) , R*np.sin(tt)])
+            rotmat = np.array([[np.cos(ph), -np.sin(ph), 0] , [np.sin(ph), np.cos(ph), 0], [0, 0, 1]])
+            rotxz = np.dot(rotmat,xzplane)
+            ax.plot(rotxz[0,:], rotxz[1,:], rotxz[2,:],lc,linewidth=lw);
+            aa = lat/deg;
+            ax.plot(R*np.cos(aa)*np.cos(tt), R*np.cos(aa)*np.sin(tt), R*np.sin(aa)*np.ones(gpts),lc,linewidth =lw);
+            
+            # plots some extra lines for reference
+            ax.plot([0, X2],[0, Y2],[0, 0],lc, linewidth= lw2);
+            ax.plot([X, X],[Y, Y],[0, Z],lc, linewidth= lw2);
+            ax.plot([0, X],[0, Y],[Z, Z],lc, linewidth= lw2);
+        #plt.title('\u03C6 = %.2f, \u03B8 = %.2f' % (ph*deg, th*deg))
+        ax.view_init(10, 10)
+    ax.grid(False)
+    ax.xaxis.set_pane_color((0.0, .0, 0.0, 0.0))
+    ax.yaxis.set_pane_color((0.0, .0, 0.0, 0.0))
+    ax.zaxis.set_pane_color((0.0, .0, 0.0, 0.0))
+    #plt.show()
+
+############################################################
+
+def seis2GR(mag, dmag, idisplay=1, ifigure=0):
+    """ Python version of seis2GR.m by Carl Tape.
+        Converts seismicity catalog to Gutenberg-Richter frequency-magnitude distribution, in both a cumulative
+        and not cumulative version.
+        
+        INPUT:
+            mag - ndarray of magnitudes for a set of events
+            dmag - magnitude increment for bins in histogram
+        OPTIONAL:
+            idisplay - display numbers in each bin (default=1)
+            ifigure - plot different versions of histogram (default=0)(See also GR2plot.m(THIS SHOULD BE IN PYTHON EVENTUALLY))
+        
+        OUTPUT:
+            N - cumulative number of events (from largest to smallest magnitude)
+            Ninc - number of events per magnitude bin
+            Medges - edges of the magnitude bins
+        
+        Dependencies:
+            numpy
+            scipy.io
+            matplotlib.pyplot"""
+    
+    n = len(mag)
+    minm = min(mag)
+    maxm = max(mag)
+    
+    print('seis2GR: %i events, min M = %.3f, max M = %.3f'% (n,minm,maxm))
+    
+    emin = np.floor(minm/dmag)*dmag
+    emax = (np.ceil(maxm/dmag)+1)*dmag # +1 in case maxm is right on an edge
+    Medges = np.arange(emin,emax,dmag).tolist()
+    Medges = np.round(Medges, decimals=2)
+    
+    Ninc, bin_edges = np.histogram(mag,Medges)
+    N = np.flipud(np.flipud(Ninc).cumsum())
+    nbin = len(Ninc)
+    
+    if idisplay == 1:
+        for ii in range(nbin): #range(x) goes from 0 to x-1
+            print('bin ',ii,': Mw = [',Medges[ii],' ',Medges[ii+1],'] Ninc = ',Ninc[ii],'N = ',N[ii])
+    
+    if ifigure == 1:
+        for kk in [1, 2, 3, 4]:
+            if kk == 1:
+                D = Ninc
+                ylab = 'Number (N = '+str(n)+')'
+            if kk == 2:
+                D = np.log10(Ninc)
+                D[np.isinf(D)] = 0 #plt.hist does not work if any value is +/-inf
+                ylab = 'Log10[Number] (N = '+str(n)+')'
+            if kk == 3:
+                D = N
+                ylab = 'Cumulative[Number] (N = '+str(n)+')'
+            if kk == 4:
+                D = np.log10(N)
+                D[np.isinf(D)] = 0
+                ylab = 'Log10[Cumulative Number] (N = '+str(n)+')'
+            plt.subplot(2,2,kk)
+            plt.hist(Medges[:-1], bins=Medges, weights=D)
+            plt.xlabel('Magnitude')
+            plt.ylabel(ylab)
+        plt.show()
+        
+    return N, Ninc, Medges
+
+############################################################
+
+def smooth(a,WSZ):
+    """ Python adaptation of MATLAB smooth.m function
+        Author: Divakar on Stack Overflow
+        
+        INPUT:
+            a: Numpy 1-D array containing the data to be smoothed
+            WSZ: smoothing window size needs. Must be an odd number, as in the original implementation
+            
+        OUTPUT:
+            Numpy 1-D array of smoothed data points"""
+    
+    out0 = np.convolve(a,np.ones(WSZ,dtype=int),'valid')/WSZ    
+    r = np.arange(1,WSZ-1,2)
+    start = np.cumsum(a[:WSZ-1])[::2]/r
+    stop = (np.cumsum(a[:-WSZ:-1])[::2]/r)[::-1]
+    
+    return np.concatenate((  start , out0, stop  ))
+
+############################################################
+
+def matlab2datetime(matlab_datenum):
+        # equivalent of datestr when converting from serial number to date
+       
+        day = dt.datetime.fromordinal(int(matlab_datenum))
+        dayfrac = dt.timedelta(days=float(matlab_datenum)%1) - dt.timedelta(days = 366)
+        return day + dayfrac
+
+############################################################
+
+# python version of Matlab's sph2cart and cart2sph function
+# transforms elements of the spherical coordinate arrays azimuth, elevation, and r to Cartesian, or xyz, coordinates.
+
+def sph2cart(azimuth,elevation,r):
+    x = r * np.cos(elevation) * np.cos(azimuth)
+    y = r * np.cos(elevation) * np.sin(azimuth)
+    z = r * np.sin(elevation)
+    return x, y, z
+
+############################################################
+
+def wf_fft(wf,fNyq):
+    """ Python adaptation of wf_fft.m by Michael West
+        Necessary for GEOS626 work
+        
+        INPUT:
+            wf - Numpy array of the data points in your trace
+            fNyq - the Nyquist frequency
+        
+        OUTPUT:
+            fft_amp - Numpy array of spectral amplitudes
+            fft_phase - Numpy array of phases
+            fft_freq - Numpy array of frequencies"""
+    
+    NFFT = int(2**(np.ceil(np.log(len(wf))/np.log(2))))  # Next highest power of 2
+    FFTX = np.fft.fft(wf,n=NFFT)                       # Take fft, padding with zeros.
+    NumUniquePts = int(np.ceil((NFFT+1)/2))
+    FFTX = FFTX[0:NumUniquePts]              # throw out neg frequencies
+    MX = abs(FFTX)                           # Take magnitude of X
+    MX = MX*2                                # Multiply by 2 
+    fft_amp = MX/len(wf) 
+    
+    fft_phase = np.angle(FFTX)               # Take magnitude of X
+    
+    f = (np.arange(NumUniquePts))*2/NFFT            
+    fft_freq = f*fNyq
+    
+    return fft_amp, fft_phase, fft_freq
+
+############################################################
