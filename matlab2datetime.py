26878056bf8254dd90c44b8aefd788abcc3c2d51
Aakash Gupta <aakash10gupta@gmail.com>
2023-01-05 11:42:56 +0000

    removed functions compiled into libraries

--- matlab2datetime.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-import datetime as dt
-def matlab2datetime(matlab_datenum):
-        # equivalent of datestr when converting from serial number to date
-       
-        day = dt.datetime.fromordinal(int(matlab_datenum))
-        dayfrac = dt.timedelta(days=float(matlab_datenum)%1) - dt.timedelta(days = 366)
-        return day + dayfrac
