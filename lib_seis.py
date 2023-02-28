f94ce833498ee3e83e047dbdcc672301f907eb1c
Aakash Gupta <aakash10gupta@gmail.com>
2023-02-28 21:14:50 +0000

    added plot_event_station.py to lib_seis.py

--- lib_seis.py
+++ lib_seis.py
@@ -1,6 +1,8 @@
 # Library of functions for the GEOS 626 course at UAF
 # Contributers: Carl Tape, Nealey Sims*, Amanda McPherson*, Aakash Gupta*
 
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import datetime as dt
 import matplotlib.pyplot as plt
 import math as m
@@ -359,6 +361,53 @@ def matlab2datetime(matlab_datenum):
     
 ############################################################
 
+def plot_event_station(elat,elon,w=[],slat=[],slon=[],stas=[]):
+    '''
+    Input:
+        elat:   event latitude
+        elon:   event longitude
+        w:      stream containing station information
+        slat:   if not given w, array of station latitudes to plot
+        slon:   if not given w, array of station longitudes to plot
+        stas:   if not given w, array of station names to plot
+    '''
+    
+    # Plot global map of selected stations
+    lat_epic = elat    # Latitude
+    lon_epic = elon   # Longitude
+    
+    
+    fig = plt.figure(figsize=[15, 8])
+    ax = fig.add_subplot(1, 1, 1,
+                         projection=ccrs.AzimuthalEquidistant(central_longitude=lon_epic,
+                                                              central_latitude=lat_epic))
+    
+    ax.set_global()
+    ax.coastlines()
+    ax.gridlines()
+    ax.add_feature(cfeature.LAND,facecolor=(0,1,0))
+    ax.scatter(elon, elat, c='b',marker='*', s=275, transform=ccrs.PlateCarree())
+    if len(w)>0:
+        for i in range (len(w)):
+            slon=w[i].stats.sac.stlo
+            slat=w[i].stats.sac.stla
+            ax.scatter(slon, slat, marker='o',c='r', s=75, transform=ccrs.PlateCarree())
+            plt.text(slon - 2, slat - 2, w[i].stats.station,
+                 horizontalalignment='right',
+                 transform=ccrs.PlateCarree())
+    else:
+        for i in range(len(slat)):
+            sln=float(slon[i])
+            slt=float(slat[i])
+            sta=stas[i]
+            ax.scatter(sln, slt, marker='o',c='r', s=75, transform=ccrs.PlateCarree())
+            plt.text(sln - 2, slt - 2, sta,
+                 horizontalalignment='right',
+                 transform=ccrs.PlateCarree())
+    plt.show()
+
+############################################################    
+    
 def seis2GR(mag, dmag, idisplay=1, ifigure=0):
     """ Python version of seis2GR.m by Carl Tape.
         Converts seismicity catalog to Gutenberg-Richter frequency-magnitude distribution, in both a cumulative
