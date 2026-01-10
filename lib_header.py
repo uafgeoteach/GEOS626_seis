367af79fbcfb4b92db98c2c7010e1ed544eaf5ad
Carl Tape <carltape@gmail.com>
2026-01-10 02:42:02 -0900

    updates from GEOS627_inverse

---
+++ lib_header.py
@@ -0,0 +1,13 @@
+# default libraries
+
+# basic python tools
+import numpy as np
+import matplotlib.pyplot as plt
+
+# these are for displaying matrices nicely
+from sympy import Matrix, init_printing
+from IPython.display import display
+from lib_geos import showmat
+
+import warnings
+warnings.filterwarnings('ignore')
